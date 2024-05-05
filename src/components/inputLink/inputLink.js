9494d98e4dd2dc6284ead607ce86363b32617098
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-04 23:42:37 -0300

    inputlink

---
+++ src/components/inputLink/inputLink.js
@@ -0,0 +1,10 @@
+import { OutlinedInput } from '@mui/material';
+
+export default function InputLink()
+{
+    return(
+        <div>
+        <OutlinedInput placeholder='link' style={{borderRadius:"10px", paddingRight:"50vh"}}/>
+        </div>
+    )
+}
