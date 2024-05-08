56b3ea0532e7c05cdc965f56dc5d0f99fc9de6e0
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-07 21:09:18 -0300

    app fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import ButtonGitHub from './components/buttonGitHub/buttonGitHub';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGitHub></ButtonGitHub>
+
       </header>
     </div>
   );
