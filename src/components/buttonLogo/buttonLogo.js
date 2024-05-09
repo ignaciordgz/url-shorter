a3634775c5840f491f6426c1856bc9d13bc3fdbe
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-09 02:01:02 -0300

    logo fixes

--- src/components/buttonLogo/buttonLogo.js
+++ src/components/buttonLogo/buttonLogo.js
@@ -4,10 +4,11 @@ import { faLink } from '@fortawesome/free-solid-svg-icons';
 export default function ButtonLogo()
 {
     return (
-        <div class="w-9 h-10 rounded-md hover:bg-neutral-800 hover:scale-105 transition duration-200 ease-in-out">
-           <a class="w-9 h-10 flex justify-center items-center opacity-40 hover:opacity-100" href="https://localhost:3000" target='_blank' rel="noreferrer">
-                <FontAwesomeIcon class="w-6 h-6" icon={faLink}/>
-           </a> 
+        <div class="w-36 h-12 rounded-md hover:bg-neutral-800 hover:scale-105 transition duration-200 ease-in-out">
+           <a class="w-36 h-12 space-x-1 pl-2 flex items-center opacity-40 hover:opacity-100" href="https://localhost:3000" target='_blank' rel="noreferrer">
+                <FontAwesomeIcon class="w-6 h-6" icon={faLink} color="white"/>
+                <span class="font-medium font-poppins" style={{width:"6.5rem",fontSize:"1rem"}}>url-shorter</span>
+           </a>
         </div>
     )
 }
