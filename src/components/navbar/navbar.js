ccdaf8346982f251f41d9bd93f567fe6f32e5baa
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-09 00:57:47 -0300

    navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import ButtonLogo from "../buttonLogo/buttonLogo"
+
+export default function Navbar()
+{
+    return (
+        <nav class="flex pt-3 pb-3 pl-5 w-full h-16 bg-neutral-900">
+            <ButtonLogo></ButtonLogo>
+        </nav>
+    )
+}
