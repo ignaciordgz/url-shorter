fe0768ae15b8c7024b5285f44dae559a0c70f40e
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-09 22:59:24 -0300

    navbar advances

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,12 @@
 import ButtonLogo from "../buttonLogo/buttonLogo"
+import ButtonGitHub from "../buttonGitHub/buttonGitHub"
 
 export default function Navbar()
 {
     return (
-        <nav class="flex pt-3 pb-3 pl-5 w-full h-16 bg-neutral-900">
+        <nav class="flex pt-2 pb-2 pl-5 pr-5 w-full h-16 justify-between bg-neutral-900">
             <ButtonLogo></ButtonLogo>
+            <ButtonGitHub></ButtonGitHub>
         </nav>
     )
 }
