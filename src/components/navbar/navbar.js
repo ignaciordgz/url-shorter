71b4b7a6842a741b15befb6e6b4e556ebd1f3db9
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-09 23:19:07 -0300

    a

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,9 +4,9 @@ import ButtonGitHub from "../buttonGitHub/buttonGitHub"
 export default function Navbar()
 {
 return (
-<nav class="items-center flex pt-2 pb-2 pl-5 pr-5 w-full h-16 justify-between bg-neutral-900">
-    <ButtonLogo></ButtonLogo>
-    <ButtonGitHub></ButtonGitHub>
-</nav>
+    <nav class="items-center flex pt-2 pb-2 pl-5 pr-5 w-full h-16 justify-between bg-neutral-900">
+        <ButtonLogo></ButtonLogo>
+        <ButtonGitHub></ButtonGitHub>
+    </nav>
 )
 }
