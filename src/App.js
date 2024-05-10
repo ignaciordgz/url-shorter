af2d34c6d5f2ead67ff3fec3714fa61c1c3c37f3
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-09 23:53:12 -0300

    button transition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import ButtonEnter from './components/buttonEnter/buttonEnter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <ButtonEnter></ButtonEnter>
       </header>
     </div>
   );
