22c889257bf2cfc9b68036084a7b86552065350b
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-11 23:05:53 -0300

    butonEnter fixes

--- src/components/buttonEnter/buttonEnter.js
+++ src/components/buttonEnter/buttonEnter.js
@@ -2,8 +2,7 @@ export default function ButtonEnter()
 {
     return (
         <div class="relative">
-            <div class="absolute -inset-0 bg-gradient-to-r from-neutral-600 to-slate-600 rounded-lg blur-lg opacity-90"></div>
-                <button class="relative bg-black transition duration-200 hover:bg-neutral-900 rounded-md font-poppins font-bold" id="getlink" style={{width:"120px", height:"42px", fontSize:"20px"}}>Get Link</button>
+            <button class="relative bg-black transition duration-200 hover:bg-neutral-900 rounded-md font-poppins font-bold" id="getlink" style={{width:"120px", height:"42px", fontSize:"20px"}}>Get Link</button>
         </div>
     )
 }
