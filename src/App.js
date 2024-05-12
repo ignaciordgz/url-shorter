9083acc56f1e4f0520d7b64f8c052ec88e7722fa
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-11 23:07:14 -0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Screen from './pages/screen';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
-
+        <Screen/>
       </header>
     </div>
   );
