b3d60b79fdcf520525c49e9631754227f94d4b87
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-12 05:38:10 -0300

    ref

--- src/pages/screen.js
+++ src/pages/screen.js
@@ -1,17 +1,30 @@
-import { Grid } from "@mui/material";
+import { Grid, useScrollTrigger } from "@mui/material";
 import InputLink from "../components/inputLink/inputLink";
 import ButtonEnter from "../components/buttonEnter/buttonEnter";
 import Navbar from "../components/navbar/navbar";
 import ButtonRefresh from "../components/buttonRefresh/buttonRefresh";
 import Output from "../components/output/output";
+import { useRef, useState } from "react";
 
 export default function Screen()
 {
+    const inputRef = useRef()
+
+    const [shortUrl, setUrl] = useState("")
+
+    const handleSubmit = (e) => 
+    {
+        e.preventDefault()
+        const url = inputRef.current.value
+    }
+
     return(
-        <div class="space-y-10">
-            <InputLink></InputLink>
-            <ButtonEnter></ButtonEnter>
-            <Output></Output>
+        <div>
+            <form class='space-y-10' onSubmit={handleSubmit}>
+                <InputLink ref={inputRef}></InputLink>
+                <ButtonEnter></ButtonEnter>
+                <Output shortUrl={shortUrl}></Output>
+            </form>
         </div>
     )
 }
