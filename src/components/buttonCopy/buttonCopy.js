7147bd5efed2cd4b27602031e2bdf5acd28f1cfe
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-16 21:32:59 -0300

    title button

--- src/components/buttonCopy/buttonCopy.js
+++ src/components/buttonCopy/buttonCopy.js
@@ -12,9 +12,9 @@ export default function ButtonCopy()
 
     return (
         <div className="relative w-9 h-10 rounded-md hover:scale-105 hover:bg-neutral-800 transition ease-in-out duration-200">
-            <a className="cursor-pointer flex w-9 h-10 justify-center items-center" onClick={Copy}>
+            <button className="cursor-pointer flex w-9 h-10 justify-center items-center" title="Copy To Clipboard" onClick={Copy}>
                 <FontAwesomeIcon className="w-6 h-6" id="buttonCopy" icon={faCopy}/>
-            </a>
+            </button>
         </div>
     )
 }
