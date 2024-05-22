a76eafc86f7011bb8726f46cb73156a91fe6b797
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-22 20:08:12 -0300

    service calls fixes and screen

--- src/service/serviceCalls.js
+++ src/service/serviceCalls.js
@@ -2,12 +2,7 @@ import axios from "axios"
 
 const BASE_URL = "http://localhost:8080/api"
 
-export const axiosGetShortUrl = () => 
+export const axiosPostStoreUrl = ( {originalURL} ) => 
 {
-    return axios.get(BASE_URL + "/shortURL")
-}
-
-export const axiosPostStoreUrl = ( originalUrl ) => 
-{
-    return axios.post(BASE_URL + "/storeURL", originalUrl)
+    return axios.post(BASE_URL + "/storeURL", {originalURL})
 }
