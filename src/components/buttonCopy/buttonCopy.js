5c93c269f7064a9656cf272413080c23263677aa
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-26 05:52:38 -0300

    backend server link

--- src/components/buttonCopy/buttonCopy.js
+++ src/components/buttonCopy/buttonCopy.js
@@ -7,7 +7,7 @@ export default function ButtonCopy()
     {
         var copiedText = document.getElementById("outLink")
 
-        navigator.clipboard.writeText("localhost:8080/api/"+copiedText.value)
+        navigator.clipboard.writeText("https://url-shorter-apirest.onrender.com/api/"+copiedText.value)
     }
 
     return (
