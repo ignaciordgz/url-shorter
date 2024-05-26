3510d8d8796dc35d8151278209dbf406ac4eb592
Ignacio Rodriguez <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2024-05-26 01:15:49 -0300

    href fix

--- src/components/buttonLogo/buttonLogo.js
+++ src/components/buttonLogo/buttonLogo.js
@@ -5,7 +5,7 @@ export default function ButtonLogo()
 {
     return (
         <div class="w-36 h-12 rounded-md hover:bg-neutral-800 hover:scale-105 transition duration-200 ease-in-out">
-           <a class="w-36 h-12 space-x-1 pl-2 flex items-center opacity-40 hover:opacity-100" href="https://localhost:3000" target='_blank' rel="noreferrer">
+           <a class="w-36 h-12 space-x-1 pl-2 flex items-center opacity-40 hover:opacity-100" href="/" target='_blank' rel="noreferrer">
                 <FontAwesomeIcon class="w-6 h-6" icon={faLink} color="white"/>
                 <span class="font-medium font-poppins" style={{width:"6.5rem", fontSize:"1rem", color:"white"}}>url-shorter</span>
            </a>
